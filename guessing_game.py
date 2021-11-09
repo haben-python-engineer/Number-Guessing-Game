825d70b2dc99463218cac8edb5c5b968cabf3a12
haben-python-engineer <92596559+haben-python-engineer@users.noreply.github.com>
2021-11-09 00:40:01 -0500

    Update guessing_game.py
    Refactored code!

--- guessing_game.py
+++ guessing_game.py
@@ -29,22 +29,21 @@ def start_game():
     
     ( You can add more features/enhancements if you'd like to. )
     """
-    print('_' * 55)
-    print('Welcome! Test your skills and guess the mystery number!')
-    print('_' * 55)
-
+    print('{}\nWelcome! Test your skills and guess the mystery number!\n{}'.format('_'*55,'_'*55))
     random_number = random.randint(1, 10)
     attempts = 1
+    highscore = []
     while True:
         try:
-            guess = int(input('Take a shot and guess the mystery number. It\'s between 1 and 10: '))
-            print('')
-            if guess > 10 or guess < 1:
-                raise ValueError(f'{guess} is not within the 1-10 range')
-        except ValueError as e:
-            print(f'Oh no! That is an invalid value ({e})')
+            guess = int(input('Take a shot and guess the mystery number. It\'s between 1 and 10: \n>>> '))
+            if guess < 1 or guess > 10:
+                raise ValueError('Guess is not within the range of 1 and 10')
+        except ValueError as error:
+            print(f'Oh no! Ran into an error. ({error})')
         else:
-            if guess > random_number:
+            if guess > 10 or guess < 1:
+                print('Over range')
+            elif guess > random_number:
                 print('Oops! That guess is too high')
                 attempts += 1
                 continue
@@ -54,13 +53,19 @@ def start_game():
                 continue
             elif guess == random_number:
                 print(f'You solved the mystery! It took you {attempts} tries')
+                highscore.append(attempts)
                 random_number = random.randint(1, 10)
-                play_again = input('Would you like to play again? (Yes/No): ')
-                if play_again.lower() == 'yes':
-                   continue
-                elif play_again.lower() == 'no':
-                    print('Thanks for playing! Bye for now.')
-                    break
+                attempts = 1
+                play_again = input('Want to play again?: ').upper()
+                while play_again not in ['YES', 'NO']:
+                    play_again = input('Please select either Yes OR No too continue: ').upper()
+                else:
+                    if play_again == 'YES':
+                        print(f'\nLast HIGHSCORE was {min(highscore)}\n')
+                        guess
+                    else:
+                        print('Thanks for playing! See you next time!')
+                        break
 
 
 
